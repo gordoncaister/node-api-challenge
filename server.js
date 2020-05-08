8bdd634c62bfcc46c6cdd0ed151542a1f75641ee
Gordon Caister <gordoncaister@gmail.com>
2020-05-08 10:14:36 -0500

    setup scripts and server file

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+
+const server = express();
+
+server.use(express.json())
+
+server.use(express.json());
+
+server.get('/', (req, res) => {
+  res.send(`<h2>Let's write some middleware!</h2>`);
+});
+
+//custom middleware
+
+function logger(req, res, next) {
+  console.log(
+    `[${new Date().toISOString()}] ${req.method} to ${req.url} from ${req.get(
+      'Origin'
+    )}`
+  );
+
+  next();
+}
+
+server.use(logger);
+
+
+
+module.exports = server;
+
+
