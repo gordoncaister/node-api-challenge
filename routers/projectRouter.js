37e5decfcc2503b8d61f81aafa6d70ab4c988d43
Gordon Caister <gordoncaister@gmail.com>
2020-05-08 10:46:41 -0500

    get projects by id working

--- routers/projectRouter.js
+++ routers/projectRouter.js
@@ -8,7 +8,7 @@ server.use(express.json())
 const Actions = require("../data/helpers/actionModel")
 const Projects = require("../data/helpers/projectModel")
 
-router.get("/",(req,res)=>{
+router.get("/", validateProjectId,(req,res)=>{
     Projects.get()
         .then(projects => {
             res.status(201).json(projects)
@@ -18,7 +18,30 @@ router.get("/",(req,res)=>{
         })
 })
 
+router.get("/:id", validateProjectId,(req,res)=>{
+    Projects.get()
+        .then(project => {
+            res.status(201).json(project)
+        })
+        .catch(error => {
+            res.status(500).json({error:"There was an error fetching project"})
+        })
+})
 
+
+function validateProjectId(req,res,next){
+    Projects.get(req.params.id)
+        .then((project) => {
+            if(project){
+                next();
+            } else {
+                res.status(400).json({message: "Invalid project ID"})
+            }
+        })
+        .catch(()=> {
+            res.status(500).json({error:"There was a problem finding the project/s"})
+        })
+}
 
 
 module.exports = router;
